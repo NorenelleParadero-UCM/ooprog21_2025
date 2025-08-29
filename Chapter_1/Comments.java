30b81660efef7c95719b1218e6601beef1bb3505
NorenelleParadero-UCM <sparaderonoriel@gmail.com>
2025-08-29 20:18:48 +0800

    Add files via upload

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,16 @@
+public class Comments {
+    public static void main(String[] args) {
+        
+        // This is a single-line comment
+        System.out.println("single-line comment");
+
+        /* This is a 
+           multi-line comment */
+        System.out.println("multi-line comment");
+
+        /**
+         * This is a documentation comment
+         */
+        System.out.println("documentation comment");
+    }
+}
