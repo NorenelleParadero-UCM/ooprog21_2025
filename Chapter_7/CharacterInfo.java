2053b9899eb033058cf68d8863b78b48d277933e
NorenelleParadero-UCM <sparaderonoriel@gmail.com>
2025-11-02 22:03:28 +0800

    Add files via upload

---
+++ Chapter_7/CharacterInfo.java
@@ -0,0 +1,19 @@
+public class CharacterInfo {
+    public static void main(String[] args) {
+        char aChar = 'C';
+        System.out.println("The character is " + aChar);
+
+        if (Character.isUpperCase(aChar))
+            System.out.println(aChar + " is uppercase");
+        if (!Character.isLowerCase(aChar))
+            System.out.println(aChar + " is not lowercase");
+
+        System.out.println("After toLowerCase(), aChar is " + Character.toLowerCase(aChar));
+        System.out.println("After toUpperCase(), aChar is " + Character.toUpperCase(aChar));
+
+        if (Character.isLetterOrDigit(aChar))
+            System.out.println(aChar + " is a letter or digit");
+        if (!Character.isWhitespace(aChar))
+            System.out.println(aChar + " is not whitespace");
+    }
+}
